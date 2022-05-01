d71333b1ba862b36f69f1461b53544f33be4c6e2
FearMy0s <mirko.giannetta96@gmail.com>
2022-05-01 13:45:57 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+
+const hamburgerMenu= document.getElementsByClassName("hamburger-menu")[0];
+
+const btnOpen= document.getElementById("open-menu");
+btnOpen.addEventListener("click", 
+    function() {
+        hamburgerMenu.style = "display: block";
+    }
+); 
+
+const btnClose= document.getElementById("close-menu");
+btnClose.addEventListener('click', 
+    function(){
+        hamburgerMenu.style = "display: none";
+    }
+);
